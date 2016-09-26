6ee6f004c5555cc0512cbbd601a0100afbcb3c45
ysriram1 <ysriram@umich.edu>
2016-09-26 16:44:08 -0500

    script changed to process only perfectly classified images

--- script.py
+++ script.py
@@ -17,10 +17,24 @@ from eigenNoduleGen import *
 
 #from eigenNoduleGen import * 
 
-varLst = ['InstanceID','Labels']
+varLst = ['InstanceID','Malignancy_1','Malignancy_2', 'Malignancy_3','Malignancy_4','Labels']
 instanceIDsLabels = pd.read_csv('./LIDC_REU2015.csv', usecols=varLst) 
 
-classLsts = [[1,2,3,4,5],[1],[2],[3],[4],[5]] 
+
+# subset to only include those cases where the maliganacy values are all equal to eachother
+perfectCases = instanceIDsLabels[(instanceIDsLabels.Malignancy_1 == \
+instanceIDsLabels.Malignancy_2) & (instanceIDsLabels.Malignancy_2 ==\
+ instanceIDsLabels.Malignancy_3) & (instanceIDsLabels.Malignancy_3 == instanceIDsLabels.Malignancy_4)]
+# 98 such cases
+
+
+# Remove the cases where the value is 3
+perfectCasesNo3 = perfectCases[perfectCases.Malignancy_1 != 3]
+# 80 such cases
+# mainly all 1s, very few 5s, only 1 or 2 2s, and NO 4s
+
+
+classLsts = [[1,2],[4,5]] 
 
 eigenValDict = {}
 eigenVecDict = {}
@@ -32,7 +46,7 @@ imageMatDict = {}
 ##Generating the EigenNodules
 for iClass, classLst in enumerate(classLsts):    
     
-    imgShape, eigVals, eigVecs,fullImageMat = createEigenNodules(instanceIDsLabels, classLst, 10, screePlot = True, imgFolderLoc = './nodules')
+    imgShape, eigVals, eigVecs,fullImageMat = createEigenNodules(perfectCasesNo3, classLst, 10, screePlot = True, imgFolderLoc = './nodules')
     
     eigenValDict[tuple(classLst)] = eigVals    
     dimDict[tuple(classLst)] = imgShape    
@@ -42,12 +56,14 @@ for iClass, classLst in enumerate(classLsts):
     
     # Visualize the top 10 eigen Nodules
     for index, arr in enumerate(eigVecs):
-        imgFileName = './EigenNodulesLocalScaledwZeroCenter/'+','.join(map(str, classLst))+'_PC'+str(index+1)+'.png'
+        imgFileName = './EigenNodulesPerfectCases/'+','.join(map(str, classLst))+'_PC'+str(index+1)+'.png'
         img = genEigenImg(arr, imgShape, eigVecs.max(), eigVecs.min(), useLocalwithClip=True, normalize=False, displayImg=False)
         #cv2.imwrite(imgFileName, img) # currently this is the only func that doesnt auto normalize
         
 eigValDf = pd.DataFrame(eigenValDict)
 eigValDf.to_csv('./eigenValues.csv')
+
+
 
 ## Gen mean images
 for classLst in imageMatDict.keys():
